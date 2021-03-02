e1a86ef5cc125082dce8f4349ac5af86d4df4c12
TMoroz <i.tanya.moroz@gmail.com>
2021-03-02 02:05:07 +0200

    HW3+4

---
+++ hw3_5.py
@@ -0,0 +1,8 @@
+number1=20
+number2=30
+a=number1+number2
+m=number1*number2
+sub=number1-number2
+div=number1/number2
+print(f'addition={sum}, multiplication={m}, division={div}, subtraction={sub}')
+
---
+++ hw3_7.py
@@ -0,0 +1,8 @@
+set={1,2,3}
+set2={2,3,5}
+set
+set2
+set.intersection(set2)
+set.difference(set2)
+set2.difference(set)
+set.symmetric_difference(set2)
